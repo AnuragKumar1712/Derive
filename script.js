480c8f5b2572f7c737867080293ba9bd86e7836d
Anurag Kumar <134754947+AnuragKumar1712@users.noreply.github.com>
2024-08-10 20:33:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Show Navbar on Scrollup
+// keep track of previous scroll position
+let prevScrollPos = window.scrollY;
+
+window.addEventListener('scroll', function() {
+  // current scroll position
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    // user has scrolled up
+    document.querySelector('.navbar').classList.add('show');
+  } else {
+    // user has scrolled down
+    document.querySelector('.navbar').classList.remove('show');
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+});
+
 // Menu Button
 const hamburger = document.getElementById("_hamburger");
 const navContent = document.getElementById("_nav-content");
@@ -26,3 +46,8 @@ function animateCards(){
         }
     });
 }
+
+// CTA
+$(document).ready(function() {
+	$('.carousel').carousel();
+});
