5943879a813d83c52bcd86642c11c0d740aa8c8c
Anurag Kumar <134754947+AnuragKumar1712@users.noreply.github.com>
2024-08-12 22:29:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,6 @@ const card = document.querySelectorAll(".about-content");
 window.addEventListener("scroll",animateCards);
 
 function animateCards(){
-    // const checkingBottom = (window.innerHeight / 4) * (125/25);
     const checkingBottom = (window.innerHeight / 4) * (100/25);
     
     card.forEach((singleCard)=>{
@@ -46,8 +45,3 @@ function animateCards(){
         }
     });
 }
-
-// CTA
-$(document).ready(function() {
-	$('.carousel').carousel();
-});
